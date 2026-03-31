4aa7e91b13261980bdf2f8868403f5e908d9808c
benhv1en <184822003+benhv1en@users.noreply.github.com>
2026-03-31 19:04:47 +0700

    xóa
    tao chán tao xóa file

--- main.c
+++ main.c
@@ -1,10 +1,2 @@
 #include <stdio.h>
-
-#include "vector.h"
-int main() {
-  Vector vector;
-  ConstructVector(&vector);
-  PushBackVector(&vector, 2);
-  PushBackVector(&vector, 5);
-  PrintVector(vector);
-}
+int main() {}
